63252063791bed95c378ddcaeabd8f07f1a84218
Olesya6292 <72027766+Olesya6292@users.noreply.github.com>
2022-08-16 08:19:50 +0300

    Update Card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -19,7 +19,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const isOwn = card.owner === currentUser._id;
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `card__like-button ${
     isLiked ? "card__like-button_active" : ""
   }`;
