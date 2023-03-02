50a66a45ccb84037ef82dfc4ec4b3fed71c24885
Olesya6292 <gc92@mail.ru>
2023-03-02 17:45:51 +0300

    fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -218,7 +218,7 @@ function App() {
         navigate("/");
       })
       .catch((err) => {
-        console.info(err);
+        console.error(err.response.data);
         setInfoTooltipStatus({
           image: negativ,
           title: "Что-то пошло не так! Попробуйте ещё раз.",
